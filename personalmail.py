0bd8afd46bbcf01c54c6f49177b939ce314042f2
GEORGI-ASPIREV1\georgi <georgi.olentsenko@gmail.com>
2018-03-20 22:18:01 +0100

    Rewritten email sending code intoa class

--- personalmail.py
+++ personalmail.py
@@ -5,24 +5,45 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+class pymailnotify:
+
+	def __init__ ( self ):
+		# Get my personal email details first.
+		# Saved in a separate file. Obviously not commited.
+		
+		# Read from file
+		file = open( "personalmail.txt", "r" )
+		personal_email_details = file.read()
+		file.close()
+		
+		# email and its password are separated by a space
+		separation = personal_email_details.find(" ")
+		self.personal_email = personal_email_details[:separation]
+		self.personal_email_password = personal_email_details[separation+1:]
+		
+		print (self.personal_email, "used for notifications")
+		
+	def notify( self, message ):
+	
+		# DO not want to be logged in all the time, so logging in separately all the time
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.starttls()
+		server.login(self.personal_email, self.personal_email_password)
+		
+		msg = MIMEText(message)
+		
+		msg['Subject'] = 'Python notification'
+		msg['From'] = self.personal_email
+		msg['To'] = self.personal_email
+		
+		# Send a test mail
+		server.sendmail( "", self.personal_email, msg.as_string() )
+		server.quit()
+		
+		print ("Notification email sent")
+
 # Send a test mail to myself
 if __name__ == '__main__':
 
-	# Get my personal email details first. Obviously not on github
-	file = open("personalmail.txt", "r")
-	personal_email_details = file.read()
-	file.close()
-	separation = personal_email_details.find(" ")
-	personal_email = personal_email_details[:separation]
-	personal_email_app_password = personal_email_details[separation+1:]
-	
-	# Sign into a gmail server
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login(personal_email, personal_email_app_password)
-
-	# Send a test mail
-	server.sendmail(personal_email, personal_email, "Just testing the mail sending capability of a python script")
-	server.quit()
-	
-	print ("Test email sent")
+	mail_service = pymailnotify()
+	mail_service.notify("Just testing the mail sending capability of a python script")
